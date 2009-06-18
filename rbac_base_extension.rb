edb33c8dd1b1d355cd525f5b065da0411e5d1ce0
Daniel K. O'Connell <dko@mac.com>
2009-06-18 22:00:44 +0800

    added require dependency application_controller for (0.8.0) release.
    Signed-off-by: Jim Gay <jim@saturnflyer.com>

--- rbac_base_extension.rb
+++ rbac_base_extension.rb
@@ -1,5 +1,5 @@
 # Uncomment this if you reference any of your controllers in activate
-require_dependency 'application'
+require_dependency 'application_controller'
 
 class RbacBaseExtension < Radiant::Extension
   version "1.2"
