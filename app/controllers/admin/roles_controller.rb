8fddff57ab2d41673d5e71417d9eab21178615a2
Jim Gay <jim@saturnflyer.com>
2010-07-05 14:33:16 -0400

    DRYer method for standard roles

--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -39,7 +39,7 @@ class Admin::RolesController < ApplicationController
   
   def destroy
     @role = Role.find(params[:id])
-    @role.destroy unless Role::RADIANT_STANDARDS.include?(@role.role_name)
+    @role.destroy unless @role.standard?
     redirect_to admin_roles_path()
   rescue ActiveRecord::RecordNotFound
     flash[:error] = 'The specified Role could not be found.'
