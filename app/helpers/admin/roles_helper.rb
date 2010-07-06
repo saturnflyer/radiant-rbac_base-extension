1bf3ff3e3d03d8f5f781452369bf50912ecd50ad
Jim Gay <jim@saturnflyer.com>
2010-07-05 21:06:10 -0400

    updated style for UI

--- app/helpers/admin/roles_helper.rb
+++ app/helpers/admin/roles_helper.rb
@@ -1,8 +1,5 @@
 module Admin::RolesHelper
   def role_spinner(which)
-    image('spinner.gif', 
-            :class => 'busy', :id => "busy_#{which}", 
-            :alt => "",  :title => "", 
-            :style => 'display: none;')
+    image('spinner.gif', :alt => "loading...")
   end
 end
